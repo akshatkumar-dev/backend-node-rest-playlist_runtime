299a3d4755dfb83a0b154c21d6100ad4f3fcec97
Akshat Kumar <akshatkumar.dev@gmail.com>
2020-03-20 16:19:17 +0530

    Implement time conversion and scrapping for playlist title and thumbnail image

--- index.js
+++ index.js
@@ -7,10 +7,16 @@ const apis = require("./apikeys");
 app.use(bodyParser.json());
 
 app.get("/api/getruntime",async function(req,res){
-    var playlistId = req.query.url.split("=")[1];
+    var url = req.query.url;
+    var playlistId = url.split("=")[1];
+    var response = await axios.get(url);
+    var data = response.data;
+    var title = $($("h1 a",data).toArray()[0]).text();
+    var img = $("td img[data-thumb]",data).toArray()[0].attribs["data-thumb"];
     //"https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&maxResults=50&playlistId=PLBCF2DAC6FFB574DE&key=[YOUR_API_KEY]
     var videoIds = []
-    var response = await axios.get(`https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults=50&playlistId=${playlistId}&key=${apis[0]}`);
+    response = await axios.get(`https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults=50&playlistId=${playlistId}&key=${apis[0]}`);
+    var totalResults = response.data["pageInfo"]["totalResults"]
     while(true){
         var items = response.data["items"];
         items.forEach(element => {
@@ -72,8 +78,63 @@ app.get("/api/getruntime",async function(req,res){
                 console.log(err)
             }
     }
-    
-    res.send(timeArray);
+    var time = convertTime(timeArray)
+    var toSend = {title: title,img: img,time:time,totalResults:totalResults}
+    res.send(toSend);
 })
+function convertTime(x){
+
+    var y = x.map(function(element){
+        element= element.replace("P","");
+        element = element.replace("T","");
+        return element;
+    })
+    var totalSeconds = 0
+    y.forEach(element=>{
+        var days = element.split("D");
+        var x;
+        
+        if(days.length == 2){
+            totalSeconds+=parseInt(days[0],10)*3600*24;
+            x = days[1]
+        }
+        else{
+            x = days[0];
+        }
+        var hours = x.split("H");
+        if(hours.length == 2){
+            totalSeconds+=parseInt(hours[0],10)*3600;
+            x = hours[1];
+        }
+        else{
+            x = hours[0]
+        }
+        var minutes = x.split("M");
+        if(minutes.length == 2){
+            totalSeconds+=parseInt(minutes[0],10)*60;
+            x = minutes[1];
+        }
+        else{
+            x = minutes[0]
+        }
+        var seconds = x.split("S");
+        if(seconds.length == 2){
+            totalSeconds+=parseInt(seconds[0],10);
+            x = seconds[1];
+        }
+        else{
+            x = seconds[0]
+        }
+        
+    })
+    var hours   = Math.floor(totalSeconds / 3600);
+    var minutes = Math.floor((totalSeconds - (hours * 3600)) / 60);
+    var seconds = totalSeconds - (hours * 3600) - (minutes * 60);
+    
+    if (hours   < 10) {hours   = "0"+hours;}
+    if (minutes < 10) {minutes = "0"+minutes;}
+    if (seconds < 10) {seconds = "0"+seconds;}
+    return (hours+':'+minutes+':'+seconds);
+}
 
 app.listen(process.env.PORT||4000,function(){console.log("listening");})
